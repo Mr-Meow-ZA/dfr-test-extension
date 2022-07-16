cb94aeecede950bda6684553133e39327b3d98aa
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 18:51:01 +0200

    changed cm variable to mm for accuracy. encoder functions now return slot count.

--- main.ts
+++ main.ts
@@ -1,13 +1,39 @@
+function turnLeft90 () {
+    clearEncoders()
+    while (5 >= encoder1() || 5 >= encoder2()) {
+        rekabit.runMotor(MotorChannel.M1, MotorDirection.Forward, 128)
+        rekabit.runMotor(MotorChannel.M2, MotorDirection.Backward, 128)
+        rekabit.brakeMotor(MotorChannel.M1)
+        rekabit.brakeMotor(MotorChannel.M2)
+    }
+}
+// checks that state has changed before incrementing, function returns cm travelled.
 function encoder1 () {
     if (stateSaved1 != pins.digitalReadPin(DigitalPin.P0)) {
         if (pins.digitalReadPin(DigitalPin.P0) == 1) {
             counter1 += pins.digitalReadPin(DigitalPin.P0)
-            serial.writeValue("x", counter1)
-            serial.writeValue("cm", 1.038 * counter1)
         }
         stateSaved1 = pins.digitalReadPin(DigitalPin.P0)
     }
-    return 1.038 * counter1
+    return counter1
+}
+function backward_for (mm: number) {
+    clearEncoders()
+    while (mm >= stepsToMilimeters(encoder1()) || mm >= stepsToMilimeters(encoder2())) {
+        rekabit.runMotor(MotorChannel.M1, MotorDirection.Backward, 128)
+        rekabit.runMotor(MotorChannel.M2, MotorDirection.Backward, 128)
+        rekabit.brakeMotor(MotorChannel.M1)
+        rekabit.brakeMotor(MotorChannel.M2)
+    }
+}
+function forward_for (mm: number) {
+    clearEncoders()
+    while (mm >= stepsToMilimeters(encoder1()) || mm >= stepsToMilimeters(encoder2())) {
+        rekabit.runMotor(MotorChannel.M1, MotorDirection.Forward, 128)
+        rekabit.runMotor(MotorChannel.M2, MotorDirection.Forward, 128)
+        rekabit.brakeMotor(MotorChannel.M1)
+        rekabit.brakeMotor(MotorChannel.M2)
+    }
 }
 function clearEncoders () {
     counter1 = 0
@@ -17,17 +43,8 @@ function clearEncoders () {
 }
 function turnRight90 () {
     clearEncoders()
-    while (5 >= encoder1() / 1.038 || 5 >= encoder2() / 1.038) {
+    while (5 >= encoder1() || 5 >= encoder2()) {
         rekabit.runMotor(MotorChannel.M1, MotorDirection.Backward, 128)
-        rekabit.runMotor(MotorChannel.M2, MotorDirection.Forward, 128)
-        rekabit.brakeMotor(MotorChannel.M1)
-        rekabit.brakeMotor(MotorChannel.M2)
-    }
-}
-function forward_for (cm: number) {
-    clearEncoders()
-    while (cm >= encoder1() || cm >= encoder2()) {
-        rekabit.runMotor(MotorChannel.M1, MotorDirection.Forward, 128)
         rekabit.runMotor(MotorChannel.M2, MotorDirection.Forward, 128)
         rekabit.brakeMotor(MotorChannel.M1)
         rekabit.brakeMotor(MotorChannel.M2)
@@ -37,21 +54,13 @@ function encoder2 () {
     if (stateSaved2 != pins.digitalReadPin(DigitalPin.P9)) {
         if (pins.digitalReadPin(DigitalPin.P9) == 1) {
             counter2 += pins.digitalReadPin(DigitalPin.P9)
-            serial.writeValue("x", counter2)
-            serial.writeValue("cm", 1.038 * counter2)
         }
         stateSaved2 = pins.digitalReadPin(DigitalPin.P9)
     }
-    return 1.038 * counter2
+    return counter2
 }
-function backward_for (cm: number) {
-    clearEncoders()
-    while (cm >= encoder1() || cm >= encoder2()) {
-        rekabit.runMotor(MotorChannel.M1, MotorDirection.Backward, 128)
-        rekabit.runMotor(MotorChannel.M2, MotorDirection.Backward, 128)
-        rekabit.brakeMotor(MotorChannel.M1)
-        rekabit.brakeMotor(MotorChannel.M2)
-    }
+function stepsToMilimeters (count: number) {
+    return 10.38 * count
 }
 let stateSaved2 = 0
 let counter2 = 0
@@ -59,7 +68,7 @@ let counter1 = 0
 let stateSaved1 = 0
 clearEncoders()
 basic.forever(function () {
-    forward_for(20)
+    forward_for(200)
     basic.pause(1000)
     turnRight90()
     basic.pause(1000)
