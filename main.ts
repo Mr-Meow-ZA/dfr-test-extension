5c07542b57819375b330e8abe741d8a2fbe545a3
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 18:38:19 +0200

    good version.

--- main.ts
+++ main.ts
@@ -9,8 +9,24 @@ function encoder1 () {
     }
     return 1.038 * counter1
 }
+function clearEncoders () {
+    counter1 = 0
+    counter2 = 0
+    stateSaved1 = pins.digitalReadPin(DigitalPin.P0)
+    stateSaved2 = pins.digitalReadPin(DigitalPin.P9)
+}
+function turnRight90 () {
+    clearEncoders()
+    while (5 >= encoder1() / 1.038 || 5 >= encoder2() / 1.038) {
+        rekabit.runMotor(MotorChannel.M1, MotorDirection.Backward, 128)
+        rekabit.runMotor(MotorChannel.M2, MotorDirection.Forward, 128)
+        rekabit.brakeMotor(MotorChannel.M1)
+        rekabit.brakeMotor(MotorChannel.M2)
+    }
+}
 function forward_for (cm: number) {
-    while (30 >= encoder1() || 30 >= encoder2()) {
+    clearEncoders()
+    while (cm >= encoder1() || cm >= encoder2()) {
         rekabit.runMotor(MotorChannel.M1, MotorDirection.Forward, 128)
         rekabit.runMotor(MotorChannel.M2, MotorDirection.Forward, 128)
         rekabit.brakeMotor(MotorChannel.M1)
@@ -29,7 +45,8 @@ function encoder2 () {
     return 1.038 * counter2
 }
 function backward_for (cm: number) {
-    while (30 >= encoder1() || 30 >= encoder2()) {
+    clearEncoders()
+    while (cm >= encoder1() || cm >= encoder2()) {
         rekabit.runMotor(MotorChannel.M1, MotorDirection.Backward, 128)
         rekabit.runMotor(MotorChannel.M2, MotorDirection.Backward, 128)
         rekabit.brakeMotor(MotorChannel.M1)
@@ -37,14 +54,13 @@ function backward_for (cm: number) {
     }
 }
 let stateSaved2 = 0
-let stateSaved1 = 0
 let counter2 = 0
 let counter1 = 0
-counter1 = 0
-counter2 = 0
-stateSaved1 = pins.digitalReadPin(DigitalPin.P0)
-stateSaved2 = pins.digitalReadPin(DigitalPin.P9)
+let stateSaved1 = 0
+clearEncoders()
 basic.forever(function () {
     forward_for(20)
-    backward_for(20)
+    basic.pause(1000)
+    turnRight90()
+    basic.pause(1000)
 })
